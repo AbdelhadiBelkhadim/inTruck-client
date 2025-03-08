b46b1fe5ca6a48fe36a2aaf7f8c6cfbbc2ba440c
Said Azmour <azmour2016maroc@gmail.com>
2025-03-08 03:02:21 +0000

    fixed features rounded and navbar position

--- src/componants/Features/FeaturesCartes.jsx
+++ src/componants/Features/FeaturesCartes.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import Features1 from '../../assets/Features1.png'
-import Features2 from '../../assets/Features2.png'
-import Features3 from '../../assets/Features3.png'
-import Features4 from '../../assets/Features4.png'
+import React from 'react';
 
-
+import Features1 from '../../assets/Features1.png';
+import Features2 from '../../assets/Features2.png';
+import Features3 from '../../assets/Features3.png';
+import Features4 from '../../assets/Features4.png';
 
 const FCartes = [
     {
@@ -35,7 +34,7 @@ const FeaturesCartes = () => {
     <div className='grid grid-cols-2 md:flex gap-5 justify-center mt-6 '>
         {
             FCartes.map((carte, index) => (
-                    <div key={index} className='shadow-2xl relative text-white bg-primary rounded-b-3xl  rounded-t-full w-[126px] h-[332px] md:w-[185px] md:h-[450px] lg:w-[199px] lg:h-[482px] flex flex-col gap-5 justify-center items-center text-center'>
+                    <div key={index} className='shadow-2xl relative text-white bg-primary rounded-t-[100px] rounded-b-3xl   w-[126px] h-[332px] md:w-[185px] md:h-[450px] lg:w-[199px] lg:h-[482px] flex flex-col gap-5 justify-center items-center text-center'>
                         <div className='bg-white flex items-center justify-center w-[112px] h-[112px] md:w-[130px] md:h-[130px] lg:w-[174px] lg:h-[174px] rounded-full absolute top-2 md:top-4'>
                             <img src={carte.img} />
                         </div>
--- src/componants/Navbar/Navbar.jsx
+++ src/componants/Navbar/Navbar.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
-import { FaBars, FaTimes } from 'react-icons/fa';
+
+import {
+  FaBars,
+  FaTimes,
+} from 'react-icons/fa';
+
 import logo from '../../assets/logo.png';
 import Booking from './Booking';
 import Getstart from './Getstart';
@@ -35,7 +40,7 @@ const Navbar = () => {
     };
 
     return (
-        <div className=" shadow-2xs p-4">
+        <div className=" shadow-2xs p-4 fixed top-0 left-0 right-0 z-50 bg-white">
             <div className="container flex justify-between items-center text-center">
                 <div id="Logo">
                     <img src={logo} alt="Logo" href='/' className='' />
