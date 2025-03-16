7f391da22fa2e9e19fad3262106385710e41356e
AbdelhadiBelkhadim <belkhdim246@gmail.com>
2025-03-17 00:03:20 +0100

    finish Home Page

--- src/componants/Testimonials/Testimonials.jsx
+++ src/componants/Testimonials/Testimonials.jsx
@@ -1,19 +1,90 @@
-import React from 'react'
-import { FaArrowLeft } from "react-icons/fa";
-import { FaArrowRight } from "react-icons/fa";
+import React, { useState, useEffect } from 'react';
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import TestimonialsCarte from './TestimonialsCarte';
+import profile1 from '../../assets/profile1.png';
+import profile2 from '../../assets/profile2.png';
+import profile3 from '../../assets/profile3.png';
 
+const testimonials = [
+  {
+    img: profile1,
+    name: 'Hannah Schmitt',
+    title: 'Lead Designer',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim.',
+  },
+  {
+    img: profile2,
+    name: 'Imane Fikri',
+    title: 'Developer',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim.',
+  },
+  {
+    img: profile3,
+    name: 'Amal Rachidi',
+    title: 'UI/UX',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim.',
+  },
+];
 
 const Testimonials = () => {
+  const [index, setIndex] = useState(0);
+
+  const nextTestimonial = () => {
+    setIndex((prevIndex) => (prevIndex + 1) % testimonials.length);
+  };
+
+  const prevTestimonial = () => {
+    setIndex((prevIndex) =>
+      prevIndex === 0 ? testimonials.length - 1 : prevIndex - 1
+    );
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      nextTestimonial();
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [index]);
+
   return (
     <div>
-      <h1 className='font-semibold text-primary text-center my-20 text-[85px]'>Testimonials</h1>
-      <div className='flex justify-between items-center text-primary px-80'>
-        <FaArrowLeft />
-        <p className='text-[26px] font-semibold'>What Our Clients Say About Us</p>
-        <FaArrowRight />
+      <h1 className='font-semibold text-primary text-center my-20 text-[36px] md:text-[85px]'>Testimonials</h1>
+
+      {/* Navigation Section */}
+      <div className='flex justify-between items-center text-primary px-12 md:px-30 lg:px-80'>
+        <button onClick={prevTestimonial} className='text-[15px] md:text-[26px] font-semibold'>
+          <FaArrowLeft />
+        </button>
+        <p className='text-[14px] md:text-[26px] font-semibold'>What Our Clients Say About Us</p>
+        <button onClick={nextTestimonial} className='text-[15px] md:text-[26px] font-semibold'>
+          <FaArrowRight />
+        </button>
+      </div>
+
+      {/* Testimonials Display */}
+      <div className="flex justify-center items-center w-full">
+        <div className="hidden md:flex md:justify-center md:items-center md:w-full ">
+          <div className="w-[90%] h-[90%] transform scale-90">
+            <TestimonialsCarte testimonial={testimonials[(index - 1 + testimonials.length) % testimonials.length]} />
+          </div>
+          <div className="w-full h-full">
+            <TestimonialsCarte testimonial={testimonials[index]} />
+          </div>
+          <div className="w-[90%] h-[90%] transform scale-90">
+            <TestimonialsCarte testimonial={testimonials[(index + 1) % testimonials.length]} />
+          </div>
+        </div>
+
+        <div className="md:hidden">
+          <TestimonialsCarte testimonial={testimonials[index]} />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Testimonials
+export default Testimonials;
--- src/componants/Testimonials/TestimonialsCarte.jsx
+++ src/componants/Testimonials/TestimonialsCarte.jsx
@@ -1,41 +1,31 @@
-import React from 'react'
-import profile1 from '../../assets/profile1.png'
-import profile2 from '../../assets/profile2.png'  
-import profile3 from '../../assets/profile3.png'  
+import React from 'react';
+import Vector5 from '../../assets/Vector 5.png';
+import Vector6 from '../../assets/Vector 6.png';
 
-
-const Data = [
-    {
-        img : {profile1},
-        name : "Hannah Schmitt",
-        title : "lead designer",
-        description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim "
-    },
-
-    {
-        img : {profile2},
-        name : "Hannah Schmitt",
-        title : "lead designer",
-        description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim "
-    },
-
-    {
-        img : {profile3},
-        name : "Hannah Schmitt",
-        title : "lead designer",
-        description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim "
-    }
-]
-
-const TestimonialsCarte = () => {
+const TestimonialsCarte = ({ testimonial }) => {
   return (
-    <div>
-        <div>
-            
-        </div>
-        
-    </div>
-  )
-}
+    <div className="relative flex justify-center items-center my-15 w-[250px] h-[250px] md:w-[423px] md:h-[454px] ">
+      <img src={Vector6} className="absolute inset-0 mx-auto" />
 
-export default TestimonialsCarte
+      <div className="relative">
+        <img src={Vector5} className="relative z-10 mr-15 drop-shadow-2xl" />
+
+        <div  className="absolute -top-5 md:left-55 md:top-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2 z-20 text-center flex flex-col justify-center items-center w-full "
+        >
+          <img src={testimonial.img} alt={testimonial.name} className=" rounded-full  border-4 border-white shadow-md w-[58px] h-[58px] md:w-[100px] md:h-[100px]"
+          />
+
+          <h3 className="text-[13px] md:text-lg font-bold text-blue-900 mt-4">
+            {testimonial.name}
+          </h3>
+          <p className="text-sm text-gray-500">{testimonial.title}</p>
+          <p className="text-gray-700 mt-3 mb-40 w-[205px] h-[99px] md:w-[317px] md:h-[166px] text-[11px] md:text-[18px]">
+            {testimonial.description}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TestimonialsCarte;
