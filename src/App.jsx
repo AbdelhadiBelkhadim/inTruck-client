745c2f789fec5515bcf347e132cabdb443963273
AbdelhadiBelkhadim <belkhdim246@gmail.com>
2025-03-26 23:57:21 +0100

    fix 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Dashboard from './pages/Dashboard.jsx'; // Ensure correct file extension
+import Dashboard from '../src/pages/Dashboard'; 
 import NotFound from './pages/NotFound';
 
 const App = () => {
