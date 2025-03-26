6e1ff6e72b103e1f4c063b2c82dd7e493b62a467
AbdelhadiBelkhadim <belkhdim246@gmail.com>
2025-03-26 23:32:14 +0100

    fix 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';
 import NotFound from './pages/NotFound';
 import Layout from './layout/Layout';
 import TrackingMain from './componants/sections/dashboardMain/trackingMain'; // Ensure TrackingMain is imported
