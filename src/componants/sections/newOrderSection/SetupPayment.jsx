d4dffe454396b2f28c04f76c6936f92938b0e25a
Bensalem Hamza <bensalemhamza382@gmail.com>
2025-04-28 03:58:34 +0200

    change in rder section

--- src/componants/sections/newOrderSection/SetupPayment.jsx
+++ src/componants/sections/newOrderSection/SetupPayment.jsx
@@ -8,20 +8,6 @@ const SetupPayment = () => {
 
   return (
     <div className='bg-[#F2F2F2] w-full min-h-screen container mx-auto'>
-      {/* Navigation Header */}
-      <div className="flex justify-between px-3 py-2">
-        <button className="flex items-center text-[#00b4d8]">
-          <ArrowLeft className="w-6 h-6 text-primary" />
-          <span className="ml-1 text-lg font-medium underline">Back</span>
-        </button>
-        <Link to="/">
-          <div id='/' className={`hidden md:flex items-center justify-center`}>
-            <img src={Logo} className='w-15 h-15'/>
-            <h1 className={`md:text-2xl text-primary font-bold ${open}`}>InTruck</h1>
-          </div>
-        </Link>
-        <button className="text-primary text-lg font-medium underline">Cancel Payment</button>
-      </div>
 
       <div className='w-full h-full flex flex-col items-center justify-center'>
         {/* Main Content */}
